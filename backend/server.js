0457785d471ba7406e1b5364c549c534404d37b9
adithya <adithyanuzpratamaputra@gmail.com>
2021-05-27 07:39:37 +0700

    upload heroku

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import path from 'path';
 import dotenv from 'dotenv';
 import data from './data.js'
+// import path from 'path';
 // import mongoose from 'mongoose';
 // import userRouter from './routers/userRouter.js';
 // import alatRouter from './routers/alatRouter.js';
@@ -13,11 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-// mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/virtualbiologilab', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-// });
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/portfolio', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,21 +26,21 @@ app.use(express.urlencoded({ extended: true }));
 // app.use('/api/users', userRouter);
 // app.use('/api/alat', alatRouter);
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-// app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-// );
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.get('/api/portfolio', (req, res) => {
     res.send(data.portfolio);
   });  
 
-app.get('/', ( req, res ) => {
-    res.send('Server  is ready');
-});
+// app.get('/', ( req, res ) => {
+//     res.send('Server  is ready');
+// });
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message })
